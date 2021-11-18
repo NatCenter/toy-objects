b3750c40dfbe0ea5e13e51231465b993223bf9a5
Nathan Center <nathanjcenter@gmail.com>
2021-11-18 14:11:51 -0600

    nc-addedIfandCleanedUpCode

--- leonids-toys.js
+++ leonids-toys.js
@@ -56,10 +56,9 @@ const addToyToInventory=(toyObject)=>{
   const toyIndex=toys.length-1
   // get the last object in an array
 const lastToy=toys[toyIndex]
-//get the id property object in the array
-const maxToyId=lastToy.id
-// step 2: increase the current max it by 1
-const newIdForToy=maxToyId+1
+
+// step 2: get the id property of the object and increase the current max it by 1
+const newIdForToy=lastToy.id+1
 //step 3:add the id property to the toy object
 toyObject.id=newIdForToy
 // step 4:push the toy object to the array
@@ -69,51 +68,16 @@ toys.push(toyObject)
 
 addToyToInventory(newToy)
 
-//add a toy object without a function using push
-/*
-const newToy={
-    id:4,
-    color:"Black",
-    price:299.99,
-    name: "PlayStation 4",
-    inStock:true,
-    unitsSold:35,
-    amountLeft:12,
-    weight:3.00
-}
 
-const newToy2={
-    id:5,
-    color:"Red and Blue",
-    price:399.99,
-    name: "Switch",
-    inStock:true,
-    unitsSold:45,
-    amountLeft:9,
-    weight:2.00
-}
 
-const newToy3={
-    
-    color:"green",
-    price:10.99,
-    name: "plusie",
-    inStock:true,
-    unitsSold:13,
-    amountLeft:10,
-    weight:.50
-}
-toys.push(newToy,newToy2,newToy3)
-const findId=2
-if (toy.id ===findId){
-    console.log(toy.id)
-}
-*/
+// filtered the array if the price is greater than 19.00
+
 
 for (const toy of toys){
 
+       
     
-    toy.price=toy.price*.5
-    console.log(` The ${toy.color} ${toy.name} costs ${toy.price} id ${toy.id}`);
-    
+    if(toy.price>=19.00){
+        console.log(` The ${toy.color} ${toy.name} costs ${toy.price} id: ${toy.id}`);
+    }
 }
